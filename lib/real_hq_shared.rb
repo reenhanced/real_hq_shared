51508c0132892f037e823532dc830ee6418a78bc
Gavin Todes <gavin.todes@gmail.com>
2011-07-12 21:24:16 -0500

    undo previous change

--- lib/real_hq_shared.rb
+++ lib/real_hq_shared.rb
@@ -1,3 +1,3 @@
 Dir[File.expand_path(File.dirname(__FILE__) + '/real_hq_shared/*.rb')].each { |file| require file }
 
-ApplicationHelper.send(:extend, SharedHelper)
+ApplicationHelper.send(:include, SharedHelper)
