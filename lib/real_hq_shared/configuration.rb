3c36cb669bf0f49da9b6d65fe5e31fb58c5f4ace
Gavin Todes <gavin.todes@gmail.com>
2011-07-12 19:18:44 -0500

    only load config file if it exists

--- lib/real_hq_shared/configuration.rb
+++ lib/real_hq_shared/configuration.rb
@@ -6,5 +6,6 @@ class NestedOstruct
     OpenStruct.new(hash.inject({}){|r,p| r[p[0]] = p[1].kind_of?(Hash) ?  NestedOstruct.new(p[1]) : p[1]; r })
   end
 end
-
-Configs = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'config.yml')))
+                                                                  
+config_file = Rails.root.join('config', 'config.yml')
+Configs = HashWithIndifferentAccess.new(YAML.load_file(config_file)) if File.exists?(config_file)
