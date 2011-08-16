f2bddd5b5419699dfcc999aebca7e5c18f887f99
Gavin Todes <gavin.todes@gmail.com>
2011-08-16 16:04:20 -0500

    shared helper

--- lib/real_hq_shared.rb
+++ lib/real_hq_shared.rb
@@ -1,8 +1,8 @@
 # Load configuration.rb first so that Configs object is available to other mixins
 require 'real_hq_shared/configuration.rb' 
 
-require 'real_hq_shared/application_helper.rb'                                        
 require 'real_hq_shared/configuration.rb'                                        
+require 'real_hq_shared/shared_helper.rb'                                        
 require 'real_hq_shared/try_chain.rb'                                        
 
 class ActionView::Base
