405952847a53ffe5963d778d7eb5f3ce27c726f0
Gavin Todes <gavin.todes@gmail.com>
2011-11-28 19:06:26 -0600

    fix way of including mailer module

--- lib/real_hq_shared.rb
+++ lib/real_hq_shared.rb
@@ -1,6 +1,4 @@
-# Load configuration.rb first so that Configs object is available to other mixins
-require 'real_hq_shared/configuration.rb'
-
+require 'real_hq_shared/configuration.rb' # Load configuration.rb first so that Configs object is available to other mixins
 require 'real_hq_shared/campfire.rb'
 require 'real_hq_shared/git_version.rb'
 require 'real_hq_shared/mailer.rb'
@@ -8,6 +6,6 @@ require 'real_hq_shared/shared_helper.rb'
 require 'real_hq_shared/try_chain.rb'
 
 ActionView::Base.send       :include, ActionView::Helpers::RealHqShared
-
-ActionView::Base.send       :include, RealHqShared::Mailer
+            
+ActiveRecord::Base.send     :include, RealHqShared::Mailer
 ActionController::Base.send :include, RealHqShared::Mailer
--- lib/real_hq_shared/mailer.rb
+++ lib/real_hq_shared/mailer.rb
@@ -1,25 +1,38 @@
 module RealHqShared
+  
   module Mailer
+    
+    extend ActiveSupport::Concern
+    
+    module ClassMethods
 
-    def safely_deliver mailer, email, *args, &block   
-      # usage:
-      # safely_deliver(FooMailer, :email_name, arg1, arg2) do
-      #   flash[:error] = "Barf! There was an email error."
-      # end
-      begin                   
-        mailer.send(email,*args).deliver
-      rescue Exception => ex
-        # Yields to the block (if provided) if an exception occurs. 
-        # This allows us to set an error flash message or take some other action in case of an ActionMailer exception.
-        if Rails.env.production? || Rails.env.staging?
-          notify_airbrake(ex) rescue nil
-          yield if block_given?
-          true
-        else
-          raise ex
+      def safely_deliver mailer, email, *args, &block   
+        # usage:
+        # safely_deliver(FooMailer, :email_name, arg1, arg2) do
+        #   flash[:error] = "Barf! There was an email error."
+        # end
+        begin
+          mailer.send(email,*args).deliver
+        rescue Exception => ex
+          # Yields to the block (if provided) if an exception occurs. 
+          # This allows us to set an error flash message or take some 
+          # other action in case of an ActionMailer exception.
+          if Rails.env.production? || Rails.env.staging?
+            notify_airbrake(ex) rescue nil
+            yield if block_given?
+            true
+          else
+            raise ex
+          end
         end
       end
+          
+    end
+    
+    def safely_deliver mailer, email, *args, &block   
+      self.class.safely_deliver mailer, email, *args, &block
     end  
 
   end
+  
 end
