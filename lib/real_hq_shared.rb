06329558c7b6942ba67bfd40b7b3a7fd6ac22d41
Gavin Todes <gavin.todes@gmail.com>
2011-12-16 18:02:46 -0600

    fix typo

--- lib/real_hq_shared.rb
+++ lib/real_hq_shared.rb
@@ -2,9 +2,9 @@ require 'real_hq_shared/configuration.rb' # Load configuration.rb first so that 
 require 'real_hq_shared/campfire.rb'
 require 'real_hq_shared/git_version.rb'
 require 'real_hq_shared/mailer.rb'
-require 'real_hq_shared/resque_worker_methods.rb'
 require 'real_hq_shared/shared_helper.rb'
 require 'real_hq_shared/try_chain.rb'
+require 'real_hq_shared/worker_methods.rb'
 
 ActionView::Base.send       :include, ActionView::Helpers::RealHqShared
             
