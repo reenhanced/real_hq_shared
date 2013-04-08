ecb3f660222795b734fb49a3111d88abaa5b8f05
Gavin Todes <gavin.todes@gmail.com>
2013-04-08 14:40:26 -0500

    tweaked configuration.rb to enable Configs to be accessed via method call or hash accessor

--- lib/real_hq_shared/configuration.rb
+++ lib/real_hq_shared/configuration.rb
@@ -5,12 +5,34 @@ class NestedOstruct
   def self.new(hash)
     OpenStruct.new(hash.inject({}){|r,p| r[p[0]] = p[1].kind_of?(Hash) ?  NestedOstruct.new(p[1]) : p[1]; r })
   end
+
+  def [](key)
+  	self.send(key)
+  end
 end
-                                                                  
+
+class OpenStruct
+  def [](key)
+    self.send(key)
+  end
+end
+                                                                   
 config_file = Rails.root.join('config', 'config.yml')                
 
 if File.exists?(config_file)
-  Configs = HashWithIndifferentAccess.new(YAML.load_file(config_file)) 
+  configs_hash = HashWithIndifferentAccess.new(YAML.load_file(config_file))
+  Configs = NestedOstruct(configs_hash)
 else
   puts "Configs object not created because config/config.yml does not exist. Create this file if you would like to use Configs." 
 end
+
+
+# To access configs, given the following configs hash:
+# { :foo => { :bar => { :baz => "hello!" } } }
+
+# Configs[:foo][:bar][:baz]
+# Configs.foo.bar.baz
+# Configs[:foo].bar.baz
+# Configs.foo[:bar].baz
+
+# etc...
