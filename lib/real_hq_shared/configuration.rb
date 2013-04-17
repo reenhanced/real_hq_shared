a07c8c6864770760a3ac1cef81fa0e3fcea08ebd
Gavin Todes <gavin.todes@gmail.com>
2013-04-17 16:40:17 -0500

    tweaked the way configs accessor works

--- lib/real_hq_shared/configuration.rb
+++ lib/real_hq_shared/configuration.rb
@@ -20,14 +20,14 @@ class NestedHashyStruct
     self.send(key)
   end
 end
-                                                                   
-config_file = Rails.root.join('config', 'config.yml')                
+
+config_file = Rails.root.join('config', 'config.yml')
 
 if File.exists?(config_file)
   configs_hash = HashWithIndifferentAccess.new(YAML.load_file(config_file))
   Configs = NestedHashyStruct.new(configs_hash)
 else
-  puts "Configs object not created because config/config.yml does not exist. Create this file if you would like to use Configs." 
+  puts "Configs object not created because config/config.yml does not exist. Create this file if you would like to use Configs."
 end
 
 
